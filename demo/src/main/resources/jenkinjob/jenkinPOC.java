18c731bf280eef9766bde32b66487b5aa2b0b22d
Sukhminder <Sukhminder11216@gmail.com>
2016-08-13 19:40:01 +0530

    another checkin

--- demo/src/main/resources/jenkinjob/jenkinPOC.java
+++ demo/src/main/resources/jenkinjob/jenkinPOC.java
@@ -8,6 +8,7 @@ public class jenkinPOC {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello jenkins");
+System.out.println("onother");
 	}
 
 }
